43f3b643144ca3cb09aea9ef7dfbc763dbe5ce72
“saad297” <skamil60@gmail.com>
2020-06-06 15:39:06 +0530

    Added : Last Name Pattern

--- UserRegProgram.sh
+++ UserRegProgram.sh
@@ -11,3 +11,16 @@ then
 else
 	echo "invalid"
 fi
+
+echo "Enter the Last Name : "
+read lname
+
+lnamepat="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+if [[ $lname =~ $lnamepat ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
+
