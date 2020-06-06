afc611f42cd2a2d9cd06502a6df57a7cd166593d
“saad297” <skamil60@gmail.com>
2020-06-06 15:33:30 +0530

    Added : First Name Pattern

--- UserRegProgram.sh
+++ UserRegProgram.sh
@@ -1,1 +1,13 @@
 echo "Welcome to the User Registration Program."
+
+echo "Enter the First Name : "
+read fname
+
+pattern="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $fname =~ $pattern ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
